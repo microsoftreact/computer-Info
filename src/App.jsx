a0435c2435e5d9ae7b976fe8af0b87e73eeebc7c
nonoteacher <nodirbektoshpulatov123345@gmail.com>
2024-03-13 06:57:44 +0500

    rename website

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,31 @@ function App() {
       id: 1,
       title: "HP",
       text: `HP (Hewlett-Packard) kompyuterlar, Hewlett-Packard Company tomonidan ishlab chiqarilgan kompyuterlar va diger IT mahsulotlari markasidir. HP, kompyuterlardan serverlarga, printerlardan yazilimlarga qadar keng qamrovli mahsulotlar taklif etadi.HP kompyuterlarining turli modellari mavjud bo'lib, ular xizmat korxonalari, shaxsiy foydalanuvchilar, va korporativ mijozlar uchun mo'ljallangan.`,
-      image: "/public/hp.png",
+      image: "hp.png",
     },
     {
       id: 2,
       title: "Acer",
       text: `Acer, Tayvan kompaniyasi bo'lib, kompyuterlar, noutbuklar, monoblok kompyuterlar, planshetlar, monitorlar, serverlar va boshqa IT mahsulotlarini ishlab chiqaradi. Acer kompyuterlari, har xil talablarga javob bera olish uchun keng xil kategoriyalarda joylashgan.`,
-      image: "/public/acer.png",
+      image: "acer.png",
     },
     {
       id: 3,
       title: "Lenovo",
       text: `Lenovo, Xitoyda tashkil etilgan va global kompyuter va IT mahsulotlari ishlab chiqaruvchi korxonadur. Lenovo kompyuterlari, noutbuklari, ultrabuklari, stol kompyuterlari, planshetlari, serverlari va boshqa mahsulotlari orqali ko'plab turli IT mahsulotlarini taklif etadi.`,
-      image: "/public/lenovo.png",
+      image: "lenovo.png",
     },
     {
       id: 4,
       title: "Asus",
       text: `Asus, Tayvanli bir korxona bo'lib, kompyuterlar, noutbuklar, planshetlar, matbuotlar, smartfonlar, monitorlar va boshqa elektronika mahsulotlarini ishlab chiqaradi. Asus kompyuterlari va mahsulotlari, yuqori sifat, innovatsiyalar va o'yinlarga mos tajribaga ega bo'lishi bilan mashhurdir.Asus noutbuklari, ZenBook, ROG (Republic of Gamers), VivoBook seriyalari va boshqa modellardan iborat.`,
-      image: "/public/asus.png",
+      image: "asus.png",
     },
     {
       id: 5,
       title: "Microsoft",
       text: `Microsoft kompyuterlari ifodalovchi bir brend emas, balki Microsoft bir dastur ishlab chiqaruvchi va operatsion tizimlar (Windows) ishlab chiqaruvchi korporatsiyadir. Microsoftning Windows operatsion tizimi, ko'p kompyuterlarda dunyo bo'ylab qo'llaniladi.Microsoft, operatsion tizimlari Windows 10, Windows 11, Office dasturlari, Surface planshet va noutbuklari, Xbox o'yin konsollari kabi mahsulotlarini ishlab chiqaradi.`,
-      image: "/public/microsoft.png",
+      image: "microsoft.png",
     },
   ];
   return (
